c03c88259248a41d89366f8e2490c52794920b1c
yamini <yamini.com>
2025-11-07 18:47:17 +0530

    msg

--- 50-backend-alb/variables.tf
+++ 50-backend-alb/variables.tf
@@ -5,3 +5,11 @@ variable "project_name" {
 variable "environment" {
     default = "dev"
 }
+
+variable "zone_id" {
+    default = "Z06672831LSN6WUF978LV"
+}
+
+variable "domain_name" {
+    default = "yaminiaws.fun"
+}
